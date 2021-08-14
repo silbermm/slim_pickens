0466fc330d0753c77c7bab7a162dda07bf26207f
Matt Silbernagel <matt@silbernagel.dev>
2021-08-14 19:59:09 -0400

    ready for 0.1.1 release

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SlimPickens.MixProject do
   def project do
     [
       app: :slim_pickens,
-      version: "0.1.1-pre",
+      version: "0.1.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
